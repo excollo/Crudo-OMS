9e9094e48cf590e944f8686fb418d78d0550798c
yjainexcollo <yashj@excollo.com>
2025-02-24 15:28:40 +0530

    Docker file for DB is setup

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,12 +4,19 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connectDB = async () => {
-    try{
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
-    }catch(error){
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
+    try {
+      await mongoose.connect(
+        `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@localhost:27017/${process.env.MONGO_DB_NAME}?authSource=admin`,
+        {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        }
+      );
+
+      console.log("MongoDB Connected Successfully");
+    } catch (error) {
+      console.error("MongoDB Connection Failed:", error);
+      process.exit(1);
     }
 }
 
